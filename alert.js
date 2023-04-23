f52a6d931efc8c7c82bdc83adab8583b423921c7
Aditya-Devarkonda <123838871+Aditya-Devarkonda@users.noreply.github.com>
2023-04-24 02:42:39 +0530

    Adding HTLM & JS files

---
+++ alert.js
@@ -0,0 +1,128 @@
+//Initialization
+var permission = confirm(`Welcome to F.R.I.E.N.D.S quiz developed in Javascript\n
+Quiz instructions :
+There will be 10 questions in this quiz.\nIf you get 10/10 you WIN\nEnter your answer in options (a/b/c/d)\n
+Do you want to continue quiz ?`);
+var score=0;
+var userAnswer=0;
+
+
+// Processing part
+function answerChecking (question,options,quizAnswer)
+{
+//   alert(question+options);
+    userAnswer=prompt(question+options);
+  if (userAnswer!==null){
+  if(quizAnswer===userAnswer)
+  {
+    score++;
+    alert(`You are right !
+Score is: ${score}`);
+  }
+  else
+  {
+    alert(`Wrong !
+Correct answer is: ${quizAnswer}`);
+  }
+    }else{}
+}
+
+
+//Array of quiz questions
+var quizQuestion=[{
+  question:"The series Friends is set in which city?\n",
+  options: `a. Los Angeles
+b. New York City
+c. Miami
+d. Seattle\n
+Your answer is: `,
+  quizAnswer:"b",
+},{
+  question:"What pet did Ross own?\n",
+  options:`a. A dog named Keith
+b. A rabbit called Lancelot
+c. A monkey named Marcel
+d. A lizard named Alistair\n
+Your answer is: `,
+  quizAnswer:"c",
+},{
+  question:"What kind of uniform does Joey wear to Monica and Chandler’s wedding?\n",
+  options:`a. Chef
+b. Soldier
+c. Fire fighter
+d. A baseball player\n
+Your answer is: `,
+  quizAnswer:"b",
+},{
+  question:"What’s the name of Joey’s penguin?\n",
+  options:`a. Snowflake
+b. Waddle
+c. Huggsy
+d. Bobber\n
+Your answer is: `,
+  quizAnswer:"c",
+},{
+  question:"What’s the name of Janice’s first husband?\n",
+  options:`a. Gary Litman
+b. Sid Goralnik
+c. Rob Bailystock
+d. Nick Layster\n
+Your answer is: `,
+  quizAnswer:"a",
+},{
+  question:"What song is Phoebe best known for?\n",
+  options:`a. Smelly Cat
+b. Smelly Dog
+c. Smelly Rabbit
+d. Smelly Worm\n
+Your answer is: `,
+  quizAnswer:"a",
+},{
+  question:"What job does Ross have?\n",
+  options:`a. Paleontologist
+b. Artist
+c. Photographer
+d. Insurance salesman\n
+Your answer is: `,
+  quizAnswer:"a",
+},{
+  question:"What does Joey never share?\n",
+  options:`a. His books
+b. His information
+c. His food
+d. His DVDs\n
+Your answer is: `,
+  quizAnswer:"c",
+},{
+  question:"What is Chandler’s middle name?\n",
+  options:`a. Muriel
+b. Jason
+c. Kim
+d. Zachary\n
+Your answer is: `,
+  quizAnswer:"a",
+},{
+  question:"What is the name of Phoebe’s alter-ego?\n",
+  options:`a. Phoebe Neeby
+b. Monica Bing
+c. Regina Phalange
+d. Elaine Benes\n
+Your answer is: `,
+  quizAnswer:"c",
+} ];
+
+
+//Input calling 
+if (permission!=false){
+for(let i=0;i<quizQuestion.length;i++){
+  if(userAnswer!=null){
+  answerChecking(quizQuestion[i].question,quizQuestion[i].options,quizQuestion[i].quizAnswer);
+}else{}}
+
+
+//Output (Wishing user)
+if(score===10){
+    alert("Congratulation you have won this quiz !")
+}else{
+    alert("Better luck next time\nYour total score is 10/"+score);  
+}}else{}
